b2d119772a483432fcb70ebe165c6b78a9f758e5
vradita29 <vradita29@gmail.com>
2024-07-27 07:25:50 +0000

    add button  login in login page

--- lib/components/my_textfield.dart
+++ lib/components/my_textfield.dart
@@ -5,7 +5,7 @@ class MyTextField extends StatelessWidget {
   const MyTextField({
     super.key,
     required this.hintText,
-    });
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +24,7 @@ class MyTextField extends StatelessWidget {
           fillColor: Theme.of(context).colorScheme.secondary,
           filled: true,
           hintText: hintText,
-          hintStyle: TextStyle(color:Theme.of(context).colorScheme.primary),
+          hintStyle: TextStyle(color: Theme.of(context).colorScheme.primary),
         ),
       ),
     );
