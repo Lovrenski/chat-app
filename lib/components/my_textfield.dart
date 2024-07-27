2b2a612b27ec27ad4f2e02e0907953e895f9e500
vradita29 <vradita29@gmail.com>
2024-07-27 05:06:43 +0000

    make login page

---
+++ lib/components/my_textfield.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class MyTextField extends StatelessWidget {
+  final String hintText;
+  const MyTextField({
+    super.key,
+    required this.hintText,
+    });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      child: TextField(
+        decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+            borderSide:
+                BorderSide(color: Theme.of(context).colorScheme.tertiary),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide:
+                BorderSide(color: Theme.of(context).colorScheme.primary),
+          ),
+          fillColor: Theme.of(context).colorScheme.secondary,
+          filled: true,
+          hintText: hintText,
+          hintStyle: TextStyle(color:Theme.of(context).colorScheme.primary),
+        ),
+      ),
+    );
+  }
+}
